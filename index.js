19f808a46d6a463d6c22d494ec7ff6ac1a1fb260
Michael Cohen <macball2000@yahoo.com>
2022-07-29 21:39:54 -0400

    JS: Fetch from Weather API

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 console.log("JAVASCRIPT LOADED!!!");
 const weatherData = document.querySelector("#weatherData");
 const nameDisplay = document.querySelector("#locationName");
-const submitButton = document.querySelector("#submitButton");
+const searchForm = document.querySelector("#searchForm");
+const options = {
+  method: 'GET',
+  headers: {
+
+  }
+};
 const getWeather = async (id) => {
   try {
-    const res = await fetch(``);
+    const res = await fetch(`http://api.weatherapi.com/v1/current.json?key=9b9179942dd149a887903158223007&q=${id}&aqi=no`, options);
     const data = await res.json();
     console.log("RESPONSE DATA: ", data);
+    nameDisplay.innerHTML = `${data.location.name}, ${data.location.country}`
+    if(data.location.region){nameDisplay.innerHTML = `${data.location.name}, ${data.location.region}, ${data.location.country}`}
 
   } catch (e) {
     console.log(e);
   }
 };
-submitButton.addEventListener("click", function () {
-  getWeather();
+searchForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(searchForm.elements.query.value);
+  getWeather(searchForm.elements.query.value);
 });
